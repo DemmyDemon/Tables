0d997a0b4249accbd00a728ae9b30b174f60a9d8
Fredrik Vold <fredrik@webkonsept.com>
2012-04-09 18:42:48 +0200

    Massive code cleanup and update for new event system

--- src/com/webkonsept/bukkit/tables/Tables.java
+++ src/com/webkonsept/bukkit/tables/Tables.java
@@ -1,50 +1,74 @@
 package com.webkonsept.bukkit.tables;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.logging.Logger;
 
 import org.bukkit.Material;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tables extends JavaPlugin {
-	private Logger log = Logger.getLogger("Minecraft");
-	protected TablesBlockListener blockListener = new TablesBlockListener(this);
+	private final static Logger log = Logger.getLogger("Minecraft");
+	private final TablesBlockListener tableListener = new TablesBlockListener(this);
 	
-	protected HashSet<Material> place = new HashSet<Material>();
-	protected HashSet<Material> onTopOf = new HashSet<Material>();
+	private static String pluginName = "Tables";
+	private static String pluginVersion = "???";
+	private static boolean verbose = false;
 	
-	
+	public final HashMap<Material,HashSet<Material>> onTop = new HashMap<Material,HashSet<Material>>(){
+        private static final long serialVersionUID = -8296976368562050363L;
+        {
+	        put(Material.WOOD_PLATE,new HashSet<Material>(){
+                private static final long serialVersionUID = 8076632167171102845L;
+                {
+	                add(Material.GLASS);
+	                add(Material.THIN_GLASS);
+	                add(Material.IRON_FENCE);
+	            }
+	        });
+	        
+	        put(Material.STONE_PLATE,new HashSet<Material>(){
+                private static final long serialVersionUID = -4085093673043901748L;
+                {
+                    add(Material.GLASS);
+                    add(Material.THIN_GLASS);
+                    add(Material.IRON_FENCE);
+	            }
+	        });
+	        
+           put(Material.CAKE_BLOCK,new HashSet<Material>(){
+                private static final long serialVersionUID = -4085093673043901748L;
+                {
+                    add(Material.GLASS);
+                    add(Material.THIN_GLASS);
+                    add(Material.IRON_FENCE);
+                }
+            });
+	        
+	    }
+	};
+	public void onLoad() {
+	    Tables.pluginVersion = getDescription().getVersion();
+	    Tables.pluginName = getDescription().getName();
+	}
 	@Override
 	public void onEnable() {
-		PluginManager pm =getServer().getPluginManager();
-		pm.registerEvent(Event.Type.BLOCK_CANBUILD	,blockListener	,Priority.Normal	,this);
-		pm.registerEvent(Event.Type.BLOCK_PHYSICS	,blockListener	,Priority.Normal	,this);
-		
-		place.clear();
-		place.add(Material.WOOD_PLATE);
-		place.add(Material.STONE_PLATE);
-		place.add(Material.CAKE);
-		
-		onTopOf.clear();
-		onTopOf.add(Material.FENCE);
-		onTopOf.add(Material.GLASS);
+		getServer().getPluginManager().registerEvents(tableListener,this);
 		out("Enabled");
 	}
 	@Override
 	public void onDisable() {
 		out("Disabled");
 	}
-	public void out(String message) {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		log.info("[" + pdfFile.getName()+ " " + pdfFile.getVersion() + "] " + message);
+	public static void out(String message){
+		log.info("["+Tables.pluginName+" v"+Tables.pluginVersion + "] "+message);
 	}
-	public void crap(String message){
-		PluginDescriptionFile pdfFile = this.getDescription();
-		log.severe("[" + pdfFile.getName()+ " " + pdfFile.getVersion() + " CRAP] " + message);
+	public void error(String message){
+		log.severe("["+Tables.pluginName+" v"+Tables.pluginVersion + "] "+message);
 	}
-
+	public static void verbose(String message){
+	    if (Tables.verbose){
+	        log.info("["+Tables.pluginName+" v"+Tables.pluginVersion + "] "+message);
+	    }
+	}
 }
--- src/com/webkonsept/bukkit/tables/TablesBlockListener.java
+++ src/com/webkonsept/bukkit/tables/TablesBlockListener.java
@@ -3,40 +3,41 @@ package com.webkonsept.bukkit.tables;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockCanBuildEvent;
-import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPhysicsEvent;
 
-public class TablesBlockListener extends BlockListener {
+public class TablesBlockListener implements Listener {
 	private Tables plugin;
 	
 	public TablesBlockListener (Tables instance){
 		plugin = instance;
 	}
 	
+	@EventHandler
 	public void onBlockCanBuild (BlockCanBuildEvent event){
 		if (!plugin.isEnabled()) return;
 		
 		Material placing = event.getMaterial();
 		Block thisBlock = event.getBlock();
-		if (plugin.place.contains(placing)){
-			Block belowBlock = thisBlock.getFace(BlockFace.DOWN);
-			if (thisBlock.getType().equals(Material.AIR)){
-				if (plugin.onTopOf.contains(belowBlock.getType())){
-					event.setBuildable(true);
-				}
+		
+		if (plugin.onTop.containsKey(placing)){
+			Block belowBlock = thisBlock.getRelative(BlockFace.DOWN);
+			if (thisBlock.getType().equals(Material.AIR) && plugin.onTop.get(placing).contains((belowBlock.getType()))){
+				event.setBuildable(true);
 			}
 		}
 	}
 	
-	@Override
+	@EventHandler
 	public void onBlockPhysics (BlockPhysicsEvent event){
 		if (!plugin.isEnabled()) return;
 		
 		Block affected = event.getBlock();
-		if (plugin.place.contains(affected.getType())){
-			Material below = affected.getFace(BlockFace.DOWN).getType();
-			if (plugin.onTopOf.contains(below)){
+		if (plugin.onTop.containsKey(affected.getType())){
+			Material below = affected.getRelative(BlockFace.DOWN).getType();
+			if (plugin.onTop.get(affected.getType()).contains(below)){
 				event.setCancelled(true);
 			}
 		}
